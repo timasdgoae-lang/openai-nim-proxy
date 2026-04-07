260a6632f4eb9cc3ac1109b44035cd332bd85400
timasdgoae-lang <timasdgoae@gmail.com>
2026-04-07 04:46:29 -0400

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 const NIM_API_BASE = process.env.NIM_API_BASE || 'https://integrate.api.nvidia.com/v1';
 const NIM_API_KEY = process.env.NIM_API_KEY;
 
-const SHOW_REASONING = false;
+const SHOW_REASONING = true;
 const ENABLE_THINKING_MODE = false;
 
 const MODEL_MAPPING = {
